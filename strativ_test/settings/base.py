3dfdb37aeb0758bed3078aeb71e7e8a7f1075a26
PrantaChakraborty <cbpranta@gmail.com>
2024-01-26 18:15:09 +0600

    add rest_framework & update requirements.txt

--- strativ_test/settings/base.py
+++ strativ_test/settings/base.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 3rd party
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -115,3 +118,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny'
+    ]
+}
+
